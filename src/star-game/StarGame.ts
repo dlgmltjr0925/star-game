c1f4f1ce35e260d626cdce0da1c29cc715f87cc4
fine <dlgmltjr0925@gmail.com>
2022-01-02 23:45:30 +0900

    feature: StarGame class 메소드 추가

--- src/star-game/StarGame.ts
+++ src/star-game/StarGame.ts
@@ -1,11 +1,38 @@
 export default class StarGame {
-  static imageStar = (new Image().src = './assets/images/star.png');
+  /**
+   * 120s
+   */
+  static PLAY_TIME = 120;
 
-  ctx: CanvasRenderingContext2D;
-  startedAt: Date;
+  private imageStar = new Image();
+  private imageRestart = new Image();
 
-  constructor(ctx: CanvasRenderingContext2D, startedAt: Date) {
+  private ctx: CanvasRenderingContext2D;
+  private startedAt: Date = new Date();
+
+  private isTimeOver = true;
+  private timeOverTimeout: NodeJS.Timeout | null = null;
+  onTimeOver?: () => void;
+
+  constructor(ctx: CanvasRenderingContext2D) {
     this.ctx = ctx;
+
+    this.imageStar.src = './assets/images/star.png';
+    this.imageRestart.src = './assets/images/refresh.png';
+  }
+
+  start(startedAt: Date = new Date()) {
     this.startedAt = startedAt;
+    this.isTimeOver = false;
+
+    if (this.timeOverTimeout) {
+      clearTimeout(this.timeOverTimeout);
+      this.timeOverTimeout = null;
+    }
+
+    this.timeOverTimeout = setTimeout(() => {
+      this.isTimeOver = true;
+      if (this.onTimeOver) this.onTimeOver();
+    }, StarGame.PLAY_TIME * 1000);
   }
 }
